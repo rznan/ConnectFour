d03befe4e7078c7e3036e22b0b6fffe30d3e1ecc
Renan <109163037+rznan@users.noreply.github.com>
2023-12-28 18:09:44 -0300

    ConnectFour class Completed

--- Connect4/Models/ConnectFour.cs
+++ Connect4/Models/ConnectFour.cs
@@ -7,10 +7,12 @@ namespace Connect4.Models
 {
     public class ConnectFour
     {
-        private Cell[,] board = new Cell[6, 7];
+        private Cell[,] board;
         private Player[] players;
         private Player currentPlayer;
-        private static readonly Dictionary<char, int> columnsLetters = new()
+        private Player ?winner = null;
+        public Player? Winner { get => winner; }
+        private static readonly Dictionary<char, int> lettersValues = new()
         {
             {'A', 0},
             {'B', 1},
@@ -24,22 +26,98 @@ namespace Connect4.Models
         {
             // Recieve Names Through User Input
             players = new Player[2];
-            players[0] = new Player("Renan", 'O', "red");
-            players[1] = new Player("Sants", 'X', "magenta");
+            board = new Cell[6, 7];
+            currentPlayer = new Player();
 
-            currentPlayer = players[0];
-
-            board.Initialize();
+            for(int line = 0; line < board.GetLength(0); line++)
+            {
+                for(int column = 0; column < board.GetLength(1); column++)
+                {
+                    board[line, column] = new Cell();
+                }
+            }
         }
 
         public void StartGame()
         {
-            throw new NotImplementedException();
+            string[] colors = ["red", "yellow"];
+            char[] symbols = ['O', 'X'];
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine
+            (
+                "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n" +
+                "■          CONNECT4            ■\n" +
+                "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" 
+            );
+            Console.ResetColor();
+
+            // Recieves the players names
+            for(int i = 0; i < players.Length; i++)
+            {
+                Console.Write($"Nome do {i + 1}° Jogador: ");
+                string ?nome = Console.ReadLine();
+
+                if(nome is not null)
+                {
+                    players[i] = new Player(nome, symbols[i], colors[i]);
+                }
+            }
+
+            currentPlayer = players[0];
         }
 
         public void DisplayBoard()
         {
-            throw new NotImplementedException();
+            for(int line = board.GetLength(0) -1; line >= 0; line--)
+            {
+                // line separator
+                Console.WriteLine("-----------------------------");
+
+                // line contents
+                for(int column = 0; column < board.GetLength(1); column++)
+                {
+                    Console.Write($"| ");
+                    board[line, column].Write();
+                    Console.Write(" ");
+                }
+
+                // line label
+                Console.Write($"|\t");
+                
+                switch(line)
+                {
+                    case 5: 
+                        Console.Write("F");
+                        break;
+                    case 4: 
+                        Console.Write("E");
+                        break;
+                    case 3: 
+                        Console.Write("D");
+                        break;
+                    case 2: 
+                        Console.Write("C");
+                        break;
+                    case 1: 
+                        Console.Write("B");
+                        break;
+                    case 0: 
+                        Console.Write("A");
+                        break;
+                    default:
+                        Console.Write(" ");
+                        break;
+                }
+
+                Console.Write('\n');
+            }
+
+            // board footer
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("  1   2   3   4   5   6   7");
         }
 
         public bool MakeMove(string move)
@@ -47,7 +125,7 @@ namespace Connect4.Models
             if(ValidateMove(move))
             {
                 int line = int.Parse(move[1].ToString()) -1;
-                int column = columnsLetters[move[0]];
+                int column = lettersValues[move[0]];
 
                 board[line, column] = new Cell(currentPlayer.Symbol, currentPlayer.Color);
 
@@ -65,17 +143,17 @@ namespace Connect4.Models
                 int column;
                 // checks if the move is formated in the correct manner: letterNumber
                 // and validates if the letter exists on the board
-                if(!columnsLetters.Any(letter => letter.Key == move[0]) ||
+                if(!lettersValues.Any(letter => letter.Key == move[0]) ||
                    !char.IsAsciiDigit(move[1])) 
                 {
                     return false;
                 }
 
                 line = int.Parse(move[1].ToString()) -1;
-                if(!(line < 6 && line >= 0)) 
+                if(!(line < 7 && line >= 0)) 
                     return false;
 
-                column = columnsLetters[move[0]];
+                column = lettersValues[move[0]];
 
                 // checks if the square is empty
                 if(board[line, column].Content == ' ')
@@ -88,9 +166,60 @@ namespace Connect4.Models
             return false;
         }
 
-        private bool CheckWin()
+        public bool CheckWin()
         {
-            throw new NotImplementedException();
+            for (int line = 0; line < board.GetLength(0); line++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    char currentSymbol = board[line, col].Content;
+
+                    if (currentSymbol == ' ')
+                        continue;
+
+                    if (CheckSequence(line, col, 1, 0, currentSymbol) ||
+                        CheckSequence(line, col, 0, 1, currentSymbol) ||
+                        CheckSequence(line, col, 1, 1, currentSymbol) ||
+                        CheckSequence(line, col, 1, -1, currentSymbol))
+                    {
+                        winner = players[0].Symbol == currentSymbol ? players[0] : players[1];
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool CheckSequence(int startLine, int startCol, int lineDelta, int colDelta, int currentSymbol)
+        {
+            int count = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int line = startLine + i * lineDelta;
+                int col = startCol + i * colDelta;
+
+                if (line >= 0 && line < board.GetLength(0) && col >= 0 && col < board.GetLength(1))
+                {
+                    if (board[line, col].Content == currentSymbol)
+                    {
+                        count++;
+                        if (count == 4)
+                            return true;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return false;
         }
 
         private void SwichtPlayer()
